aad90f245c2ab76b540a55e27ca22e256d0953c1
Sergey Krotkih <sergey.krotkig@gmail.com>
2020-05-30 05:37:42 +0300

    refactor post preview. add model view fro it.

--- src/screens/Home/PostPreview/index.js
+++ src/screens/Home/PostPreview/index.js
@@ -5,6 +5,7 @@ import Time from '@components/Time';
 import {connect} from 'react-redux';
 import ImageViewer from '@theme/components/ImageViewer';
 import {Container, Header, Content, Text, Thumbnail} from 'native-base';
+import {ModelView} from './modelView';
 
 const {View} = ReactNative;
 const {Component} = React;
@@ -13,17 +14,8 @@ export class PostPreview extends Component {
   constructor(props) {
     super(props);
     const {item} = this.props;
-    this.state = this.createState(item);
-  }
-
-  createState(item) {
-    const state = {
-      post: item.post,
-      author: item.author,
-      avatarUrl: item.avatarUrl,
-      imageUrl: item.imageUrl,
-    };
-    return state;
+    this.mv = new ModelView(item);
+    this.state = this.mv.createState();
   }
 
   setTitle(titleText) {
@@ -32,46 +24,31 @@ export class PostPreview extends Component {
   }
 
   componentWillMount() {
-    this.setTitle(this.state.post.title);
+    this.setTitle(this.mv.title);
   }
 
   render() {
-    this.title = this.state.post.title;
-    this.body = this.state.post.body;
-    const avatarUrl = this.state.avatarUrl;
-    if (avatarUrl === null) {
-      var avatar = {uri: 'Icon-Profile'};
-    } else {
-      var avatar = {uri: avatarUrl};
-    }
-    var imageUrl = this.state.imageUrl;
-    if (imageUrl === null) {
-      imageUrl = 'Icon-Profile';
-    }
-    this.publishedAt = new Date(1000 * this.state.post.created);
-    this.userName = this.state.author.username == null ? '' : this.state.author.username;
-
     return (
       <Container style={styles.container}>
         <Header style={styles.headerBackground}>
-          <Text style={styles.title}>{this.title}</Text>
+          <Text style={styles.title}>{this.mv.title}</Text>
         </Header>
         <Content style={styles.content}>
           <View style={styles.thumbnail}>
-            <Thumbnail circular size={55} source={avatar} />
-            <Text style={styles.userName}>{this.userName}</Text>
+            <Thumbnail circular size={55} source={this.mv.avatarUrl} />
+            <Text style={styles.userName}>{this.mv.userName}</Text>
           </View>
-          <Text style={styles.description}>{this.body}</Text>
+          <Text style={styles.description}>{this.mv.body}</Text>
           <View style={styles.backgroundPhoto}>
             <ImageViewer
               disabled={false}
-              source={{uri: imageUrl}}
+              source={this.mv.imageUrl}
               downloadable
               doubleTapEnabled={true}
             />
           </View>
           <Text style={styles.publishedAt}>
-            <Time date={this.publishedAt} />
+            <Time date={this.mv.publishedAt} />
           </Text>
         </Content>
       </Container>
---
+++ src/screens/Home/PostPreview/modelView.js
@@ -0,0 +1,51 @@
+// Post preview model view
+
+export class ModelView {
+    constructor(postItem) {
+        this.item = postItem;
+    }
+
+    get title() {
+        return this.item.post.title;
+    }
+
+    get body() {
+        return this.item.post.body;
+    }
+
+    get avatarUrl() {
+        let avatarUrl = this.item.avatarUrl;
+        if (avatarUrl === null) {
+            return {uri: 'Icon-Profile'};
+        } else {
+            return {uri: avatarUrl};
+        }
+    }
+
+    get imageUrl() {
+        let imageUrl = this.item.imageUrl;
+        if (imageUrl === null) {
+            return {uri: 'Icon-Profile'};
+        } else {
+            return {uri: imageUrl};
+        }
+    }
+
+    get publishedAt() {
+        return new Date(1000 * this.item.post.created);
+    }
+
+    get userName() {
+        return this.item.author.username == null ? '' : this.item.author.username;
+    }
+
+    createState() {
+        const state = {
+            post: this.item.post,
+            author: this.item.author,
+            avatarUrl: this.item.avatarUrl,
+            imageUrl: this.item.imageUrl,
+        };
+        return state;
+    }
+}
