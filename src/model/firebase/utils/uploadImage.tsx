d6c2eac55e0b3e3e67ba9cda32b64ea8a4b5728c
Sergey Krotkih <sergey.krotkig@gmail.com>
2020-07-20 13:16:59 +0300

    prevent to upload empty file

--- src/model/firebase/utils/uploadImage.tsx
+++ src/model/firebase/utils/uploadImage.tsx
@@ -1,35 +1,41 @@
 import {Platform} from 'react-native';
 import RNFetchBlob from 'rn-fetch-blob';
 import FirebaseClient from 'firebase';
+import {isEmpty} from 'lodash';
 
 export function uploadImage(uri, id) {
-  const Blob = RNFetchBlob.polyfill.Blob;
-  const fs = RNFetchBlob.fs;
-  window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
-  window.Blob = Blob;
   return new Promise((resolve, reject) => {
-    const mime = 'application/octet-stream';
     const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
-    let uploadBlob = null;
-    let fileName = `${id}.jpg`;
-    const imageRef = FirebaseClient.storage().ref('').child(fileName);
-    fs.readFile(uploadUri, 'base64')
-      .then((data) => {
-        return Blob.build(data, {type: `${mime}BASE64`});
-      })
-      .then((blob) => {
-        uploadBlob = blob;
-        return imageRef.put(blob, {contentType: mime});
-      })
-      .then(() => {
-        uploadBlob.close();
-        return imageRef.getDownloadURL();
-      })
-      .then((url) => {
-        resolve(url);
-      })
-      .catch((error) => {
-        reject(error);
-      });
+    if (isEmpty(uploadUri)) {
+      reject('Source file name to upload is not presented!');
+    } else {
+      const Blob = RNFetchBlob.polyfill.Blob;
+      const fs = RNFetchBlob.fs;
+      window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+      window.Blob = Blob;
+      let uploadBlob = null;
+      let fileName = `${id}.jpg`;
+      const imageRef = FirebaseClient.storage().ref('').child(fileName);
+      fs.readFile(uploadUri, 'base64')
+        .then((data) => {
+          const mime = 'application/octet-stream';
+          return Blob.build(data, {type: `${mime}BASE64`});
+        })
+        .then((blob) => {
+          uploadBlob = blob;
+          const mime = 'application/octet-stream';
+          return imageRef.put(blob, {contentType: mime});
+        })
+        .then(() => {
+          uploadBlob.close();
+          return imageRef.getDownloadURL();
+        })
+        .then((url) => {
+          resolve(url);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    }
   });
 }
