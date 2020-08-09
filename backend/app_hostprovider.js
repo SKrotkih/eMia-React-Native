d4de4a1f88adb6444cacbb231b71312c7b53306d
Sergey Krotkih <sergey.krotkig@gmail.com>
2020-08-09 11:26:12 +0300

    create new project for  backend  (remove from the client project)

--- backend/app_hostprovider.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  const message = 'It works!\n',
-    version = 'NodeJS ' + process.versions.node + '\n',
-    response = [message, version].join('\n');
-  res.end(response);
-});
--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model, Types} = require('mongoose')
-
-const schema = new Schema({
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  posts: [{ type: Types.ObjectId, ref: 'Post' }]
-})
-
-module.exports = model('User', schema)
