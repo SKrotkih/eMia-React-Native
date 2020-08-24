45e0ef5e371f948f149cba03d9de5066e08c33e9
Sergey Krotkih <sergey.krotkig@gmail.com>
2020-08-24 17:45:01 +0300

    add uploading image to the Cloudinary

--- src/model/network/server/StorageDBInteractor.ts
+++ src/model/network/server/StorageDBInteractor.ts
@@ -6,10 +6,64 @@ import RNFetchBlob from "rn-fetch-blob/index";
 
 export class StorageDBInteractor implements DBStorageInteractor {
 
+  // Upload image to the Cloudinary
+
+  async uploadImage(photo: ImagePickerResponse, id: string): Promise<string> {
+    return new Promise<string>(async (resolve, reject) => {
+      if (!(photo && photo.uri)) {
+        reject(Error('The Image is not presented so it has not uploaded on server'));
+        return;
+      }
+      const data = new FormData();
+      const file = this.createFileInfo(photo, id);
+      data.append('file', file);
+      data.append('upload_preset', 'emiaapppreset');
+      data.append('cloud_name', 'emia');
+
+      console.log(data);
+
+      fetch('https://api.cloudinary.com/v1_1/emia/image/upload', {
+        method: 'post',
+        body: data,
+      })
+        .then((res) => {
+          console.log(res);
+          res.json();
+        })
+        .then((json) => {
+          console.log(json);
+          const url = json.secure_url;
+          console.log(url);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  }
+
+  createFileInfo(photo: ImagePickerResponse, id: string): Object {
+    const uri: string =
+      Platform.OS === 'android' ? photo.uri : photo.uri.replace('file://', '');
+    const type = photo.type;
+    const name = `eMia${id}.jpeg`;
+    const source = {
+      uri,
+      type,
+      name,
+    };
+    return source;
+  }
+
+  // Upload to the app server
   // Form Data
 
-  async uploadImage2(photo: ImagePickerResponse, id: string): Promise<string> {
+  async uploadImage3(photo: ImagePickerResponse, id: string): Promise<string> {
     return new Promise<string>(async (resolve, reject) => {
+      if (!(photo && photo.uri)) {
+        reject(Error('The Image is not presented so it has not uploaded on server'));
+        return;
+      }
+
       const apiUrl = BASE_URL + '/api/images/upload';
 
       const headers = {
@@ -53,17 +107,20 @@ export class StorageDBInteractor implements DBStorageInteractor {
   }
 
   // BLOB
+  // https://github.com/joltup/rn-fetch-blob#user-content-upload-example--dropbox-files-upload-api
 
-  async uploadImage(photo: ImagePickerResponse, id: string): Promise<string> {
+  async uploadImage2(photo: ImagePickerResponse, id: string): Promise<string> {
     return new Promise<string>(async (resolve, reject) => {
       const apiUrl = BASE_URL + '/api/images/upload';
       console.log('URL ' + apiUrl);
 
-      const blobData = this.createBlobData(photo, id);
       const blobHeaders = {
         Accept: 'application/json',
         'Content-Type': 'multipart/form-data',
       };
+
+      const blobData = this.createBlobData(photo, id);
+
       RNFetchBlob.fetch('POST', apiUrl, blobHeaders, blobData)
         .then((response) => response.json())
         .then((response) => {
